c09fb46144d1a7b1cd266b4214109916995e4b3a
LuisUniverseCity <68173982+LuisUniverseCity@users.noreply.github.com>
2025-01-05 23:25:17 -0600

    Update wranglerMod.js

--- wranglerMod.js
+++ wranglerMod.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const filePath = './src/wrangler.json';
+const filePath = './wrangler.json';
 
 const readline = require('readline').createInterface({
     input: process.stdin,
