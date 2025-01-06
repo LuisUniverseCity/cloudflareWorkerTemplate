0af4f89f71645a0aef8ba1629d497c9df3d7a6f5
LuisUniverseCity <68173982+LuisUniverseCity@users.noreply.github.com>
2025-01-05 23:41:47 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  * Welcome to Cloudflare Workers! Version improved.
  * 
- * - Extract all of the files inside node_modules folder
+ * - cd to /Workers
  * - Run "npm install -g wrangler" to install the wrangler CLI
  * - Run "wrangler login" to log in to your cloudflare account
  * - Run "wrangler deploy" to publish your new API or "wrangler dev" to preview locally
